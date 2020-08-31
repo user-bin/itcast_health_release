9014347814ec327950562f9fc1712e5a9aec1087
SUN <SUN@itcast.cn>
2020-08-31 23:16:11 +0800

    定时任务--入门案例完成

---
+++ health_job/src/main/java/com/itheima/job/ClearImageJob.java
@@ -0,0 +1,12 @@
+package com.itheima.job;
+
+import java.util.Date;
+
+public class ClearImageJob {
+	/**
+	 * 定义清理图片的任务
+	 */	
+	public void clearImageJob(){
+		System.out.println("clearImageJob......" + new Date());
+	}
+}
