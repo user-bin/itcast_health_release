4e101bb4153c01e8903efda73765021ab855a43a
SUN <SUN@itcast.cn>
2020-09-01 21:24:22 +0800

    poi--读取excel[类型转换]

--- health_common/src/test/java/com/itheima/PoiRead2.java
+++ health_common/src/test/java/com/itheima/PoiRead2.java
@@ -1,5 +1,6 @@
 package com.itheima;
 
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -7,7 +8,8 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author 黑马程序员
@@ -16,7 +18,8 @@ import java.io.IOException;
  */
 public class PoiRead2 {
 
-    public static void main(String[] args) throws IOException {
+    //使用POI从Excel文件读取数据
+    public static void main(String[] args) throws Exception{
         //通过输入流加载指定的文件，封装成一个POI提供的Excel文件对象
         XSSFWorkbook excel = new XSSFWorkbook(new FileInputStream(new File("C:\\Users\\sun\\Desktop\\test_poi.xlsx")));
         //读取第一个工作表中的数据
@@ -28,8 +31,41 @@ public class PoiRead2 {
             short lastCellNum = row.getLastCellNum();
             for(int j=0;j<lastCellNum;j++){
                 Cell cell = row.getCell(j);
-                System.out.print(cell);
-                System.out.print("\t");
+                //获得单元格中的数据
+                switch (cell.getCellType()){
+                    // 字符串类型
+                    case Cell.CELL_TYPE_STRING :
+                        System.out.print(cell.getStringCellValue()); break;
+                    // 布尔值： boolean
+                    case Cell.CELL_TYPE_BOOLEAN:
+                        System.out.print(cell.getBooleanCellValue()); break;
+                    // 数值类型
+                    case Cell.CELL_TYPE_NUMERIC :
+                        //数值类型中也有可能是 日期类型，如果日期类型，则按照日期类型获取
+                        if(HSSFDateUtil.isCellDateFormatted(cell)){
+                            Date date = cell.getDateCellValue();
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+                            String str = sdf.format(date);
+                            System.out.print(str);
+                        }else{
+                            cell.setCellType(Cell.CELL_TYPE_STRING);
+                            System.out.print(cell.getStringCellValue());
+                        }
+                        break;
+                    //空值
+                    case Cell.CELL_TYPE_BLANK :
+                        System.out.print("空值"); break;
+                    // 公式类型
+                    case Cell.CELL_TYPE_FORMULA :
+                        System.out.print(cell.getCellFormula()); break;
+                    //  未知错误
+                    case Cell.CELL_TYPE_ERROR :
+                        System.out.print("未知错误"); break;
+                    //未知类型
+                    default:
+                        System.out.print("未知类型"); break;
+                }
+                System.out.print("\t\t");
             }
             System.out.println();
         }
