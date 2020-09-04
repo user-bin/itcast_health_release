6617df8bc42dcc9c3fa3aed0357fe433fbd7ef00
SUN <SUN@itcast.cn>
2020-09-04 21:29:02 +0800

    预约功能--发送验证码成功

---
+++ health_mobile_backend/src/main/java/com/itheima/controller/ValidateCodeController.java
@@ -0,0 +1,46 @@
+package com.itheima.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.itheima.constant.MessageConst;
+import com.itheima.constant.RedisMessageConstant;
+import com.itheima.entity.Result;
+import com.itheima.service.ValidateCodeService;
+import com.itheima.utils.ValidateCodeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/validateCode")
+@Slf4j
+public class ValidateCodeController {
+
+    @Reference
+    ValidateCodeService validateCodeService;
+
+
+    /**
+     * 步骤
+     *   1. 调用工具类生成验证码
+     *   2. 把验证码存储到redis
+     *   3. 发送验证码到用户手机
+     *
+     * @param telephone
+     * @return
+     */
+    @RequestMapping("/send4Order")
+    public Result send4Order(String telephone){
+        String code = ValidateCodeUtils.generateValidateCode(4).toString();
+        log.debug("验证码：" + code);
+        //发送验证码到用户
+        validateCodeService.sendValidateCodeShortMessage(telephone, RedisMessageConstant.SENDTYPE_ORDER, String.valueOf(code));
+        log.debug("发送验证码成功！！");
+        return new Result(true, MessageConst.SEND_VALIDATECODE_SUCCESS);
+    }
+
+}
